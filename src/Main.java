90eda57c054fb2f02879c2857c6754f3dc3c71ad
DanilSinitsin <sinitsindanil90@mail.ru>
2022-07-15 20:48:39 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 60;
+        int bonus = 20;
+
+        int mile = (amount / bonus);
+
+        System.out.println(mile);
+    }
+}
